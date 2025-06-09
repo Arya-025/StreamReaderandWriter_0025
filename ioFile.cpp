b3e901992d68be2d52e1c98470075cfc344de879
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-09 10:18:06 +0700

    menampilkan hasil akhir

--- ioFile.cpp
+++ ioFile.cpp
@@ -3,19 +3,21 @@
 #include <string>
 using namespace std;
 
-
-int main() {
+int main()
+{
     string Baris;
 
     ofstream outfile;
 
-    outfile.open ("contoh file.txt");
+    outfile.open("contoh file.txt");
     cout << ">=menulis file, \'q' untuk keluar" << endl;
 
-    while (true){
-        cout <<"_ ";
+    while (true)
+    {
+        cout << "_ ";
         getline(cin, Baris);
-        if (Baris =="q") break;
+        if (Baris == "q")
+            break;
         outfile << Baris << endl;
     }
 
@@ -23,5 +25,19 @@ int main() {
 
     ifstream infile;
 
-    infile.open("contoh.txt");
+    infile.open("contoh file.txt");
+
+    cout << endl
+         << ">= membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, Baris))
+        {
+            cout << Baris << '\n';
+        }
+        infile.close();
+    }
+    else cout <<"unable to open file ";
+    return 0;
 }
