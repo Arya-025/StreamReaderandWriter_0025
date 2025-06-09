3c0b6902d057e10cec90eb4c708c3874aaa1b086
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-09 10:37:17 +0700

    meanmpilkan hasil akhir

---
+++ manualExeption.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        throw 0, 5;
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a)
+    {
+        cout << "pengecualian akan diexsekusi" << endl;
+    }
+    return 0;
+}
